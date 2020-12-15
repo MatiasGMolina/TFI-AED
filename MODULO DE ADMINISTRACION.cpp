d01d83e4eedf0b68c900bd47c913265757086f94
German <maurino.german@gmail.com>
2020-12-14 21:16:34 -0300

    Agregamos validacion de contrasenia

--- MODULO DE ADMINISTRACION.cpp
+++ MODULO DE ADMINISTRACION.cpp
@@ -3,11 +3,16 @@
 #include <string.h>
 #include <wchar.h>
 #include <locale.h>
+
 void menu();
 void reg_vet();
 void reg_asistente();
 bool buscar_usuario(char usu[10]);
 bool buscar_vet(char nombre[60],int matricula);
+void validar_contrasenia(char cadena[32]);
+bool letras_consecutivas(char contrasenia[32]);
+bool numeros_consecutivos(char contrasenia[32]);
+
 struct usuarios
 {
 	char usuario[10];
@@ -18,7 +23,7 @@ struct veterinario
 {	char apell_nom[60];
 	int matricula, dni;
 	char telefono[10];
-	char contrasenia[10];
+	char contrasenia[32];
 };
 main()
 {
@@ -47,7 +52,8 @@ main()
 					break;
 				case 3: 
 					
-					
+					//atenciones_vet();
+					system("\n\npause");
 					break;
 				case 4:
 
@@ -67,6 +73,9 @@ main()
 	printf("\n\n");
 	system("pause");
 }
+
+//*****************************************************************************************************//
+
 void menu()
 {
 		system("cls");
@@ -87,28 +96,16 @@ void menu()
 		printf("\t\t******************************************************************\n\n");
 }
 
-bool buscar_vet(char nombre[60],int matricula)
-{	
-	FILE*arch;
-	veterinario reg;
-	arch=fopen("veterinario.dat", "a+b");
-	fread(&reg, sizeof(reg), 1, arch);
-	while(!feof(arch))
-	{	
-		if(strcmp(reg.apell_nom, nombre)==0 or (matricula==reg.matricula))
-		{	
-			fclose(arch);
-			return true;
-		}
-	fread(&reg, sizeof(reg), 1, arch);
-	}
-	fclose(arch);
-	return false;
-}
+
+
+//*****************************************************************************************************//
+
+
 void reg_vet()
 {
 	bool h;
 	veterinario reg1;
+	int i,n=0,band=0,letra_minuscula=0,letra_mayuscula=0,caracter_numerico=0,caracter_invalido1=0;
 	
 	system("cls");
 	printf("\n\tRegistrar Veterinario.");
@@ -132,10 +129,21 @@ void reg_vet()
 	printf("\nIngrese el telefono: ");
 	scanf("%d", &reg1.telefono);
 	
-
-	printf("\nIngrese la clave: ");
+	printf("\nCondiciones para el ingreso de la contrasenia: \n");
+	printf("\nA. Debera contener al menos una letra mayuscula, una letra minuscula y un numero. ");
+	printf("\nB. No podra contener ningun caracter de puntuacion, ni acentos, ni espacios. ");
+	printf("solo caracteres alfanumericos. ");
+	printf("\nC. Debera tener entre 6 y 32 caracteres.");
+	printf("\nD. No debe tener mas de 3 caracteres numericos consecutivos. ");
+	printf("\nE. No debe tener 2 caracteres consecutivos que refieran a letras alfabeticamente consecutivas (ascendentemente).");
+	printf("Este criterio es valido tanto para letras mayusculas, minusculas o combinacion de ambas.");
+	
+	printf("\n\nIngrese la clave: ");
 	_flushall();
 	gets(reg1.contrasenia);
+	
+	validar_contrasenia(reg1.contrasenia);
+
 	
 	FILE*arch1;
 	arch1=fopen("veterinario.dat", "ab");
@@ -143,7 +151,16 @@ void reg_vet()
 	fclose(arch1);
 	printf("\nVETERINARIO REGISTRADO\n");
 	printf("\n");
+	
+	
 }
+
+
+
+//*****************************************************************************************************//
+
+
+
 void reg_asistente()
 {	
 	bool h;
@@ -237,16 +254,79 @@ void reg_asistente()
 		}
 	}
 	
-	printf("\nINGRESE LA CLAVE:");
+	printf("\nCondiciones para el ingreso de la contrasenia: \n");
+	printf("\nA. Debera contener al menos una letra mayuscula, una letra minuscula y un numero. ");
+	printf("\nB. No podra contener ningun caracter de puntuacion, ni acentos, ni espacios. ");
+	printf("solo caracteres alfanumericos. ");
+	printf("\nC. Debera tener entre 6 y 32 caracteres.");
+	printf("\nD. No debe tener mas de 3 caracteres numericos consecutivos. ");
+	printf("\nE. No debe tener 2 caracteres consecutivos que refieran a letras alfabeticamente consecutivas (ascendentemente).");
+	printf("Este criterio es valido tanto para letras mayusculas, minusculas o combinacion de ambas.");
+	
+	printf("\n\nIngrese la clave: ");
 	_flushall();
 	gets(reg.contrasenia);
 	
+	validar_contrasenia(reg.contrasenia);
+
 	FILE*arch;
 	arch=fopen("usuario.dat", "ab");
 	fwrite(&reg, sizeof(reg), 1, arch);
 	fclose(arch);
 	printf("\nASISTENTE REGISTRADO\n");
+	
 }
+
+
+//*****************************************************************************************************//
+
+
+//void atenciones_vet()
+//{
+//	int mes;
+//	FILE*archvet,*archturnos,*archvet;
+//	
+//	
+//	printf("\nIngrese el número de mes a listar: ");
+//	scanf("%d",&mes);
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}
+
+//*****************************************************************************************************//
+//*****************************************************************************************************//
+//***************                           FUNCTIONS                            **********************//
+//*****************************************************************************************************//
+//*****************************************************************************************************//
+
+
+//Retorna verdadero si el nombre del veterinario y la matricula ya existen 
+bool buscar_vet(char nombre[60],int matricula)
+{	
+	FILE*arch;
+	veterinario reg;
+	arch=fopen("veterinario.dat", "a+b");
+	fread(&reg, sizeof(reg), 1, arch);
+	while(!feof(arch))
+	{	
+		if(strcmp(reg.apell_nom, nombre)==0 or (matricula==reg.matricula))
+		{	
+			fclose(arch);
+			return true;
+		}
+	fread(&reg, sizeof(reg), 1, arch);
+	}
+	fclose(arch);
+	return false;
+}
+
+
+//Retorna verdadero si el nombre del usuario ya existe
 bool buscar_usuario(char usu[10])
 {	
 	FILE*arch;
@@ -264,4 +344,158 @@ bool buscar_usuario(char usu[10])
 	}
 	fclose(arch);
 	return false;
+}
+
+
+//validar contrasenia
+void validar_contrasenia(char cadena[32])
+{
+	int i,n=0,band=0,letra_minuscula=0,letra_mayuscula=0,caracter_numerico=0,caracter_invalido1=0;
+	
+	while(band==0)
+	{
+		n=strlen(cadena);
+		for(i=0;i<n;i++)
+		{
+			if(cadena[i] >= 'a' and cadena[i] <=  'z')
+			{
+				letra_minuscula++;
+			}
+			
+			if(cadena[i] >= 'A' and cadena[i] <= 'Z')
+			{
+				letra_mayuscula++;
+			}
+			
+			if(cadena[i] >= '0' and cadena[i] <= '9')
+			{
+				caracter_numerico++;
+			}
+			
+			if((cadena[i] == '+') or (cadena[i] == ',') or (cadena[i] == ';') or (cadena[i] == ':') or (cadena[i] == '""') 
+		 	or (cadena[i] == '[]') or (cadena[i] == '{}') or (cadena[i] == '()') or (cadena[i] == '?') 
+			or (cadena[i] == '¿') or (cadena[i] == '-') or (cadena[i] == '_') or (cadena[i] == '!') or (cadena[i] == '¡') 
+			or (cadena[i] == ' '))
+			{
+				caracter_invalido1++;
+			}	
+		}
+		
+		if(letra_minuscula < 1)
+		{
+			printf("\nERROR. La contrasenia debe contener al menos una letra minuscula.");
+			band=1;
+		}
+		if(letra_mayuscula < 1)
+		{
+			printf("\nERROR. La contrasenia debe contener al menos una letra mayuscula.");
+			band=1;
+		}
+		if(caracter_numerico < 1)
+		{
+			printf("\nERROR. La contrasenia debe contener al menos un numero.");
+			band=1;
+		}
+		if(caracter_invalido1 != 0)
+		{
+			printf("\nERROR. La contrasenia no debe contener caracteres de puntuacion, acentos, ni espacios.");
+			band=1;
+		}
+		if(strlen(cadena) < 6)
+		{
+			printf("\nERROR. La contrasenia debe tener mas de 6 caracteres.");
+			band=1;
+		}
+		if(strlen(cadena) > 32)
+		{
+			printf("\nERROR. La contrasenia debe tener menos de 32 caracteres.");
+			band=1;
+		}
+		if(numeros_consecutivos(cadena))
+		{
+			printf("\nERROR. La contrasenia no debe poseer mas de 3 caracteres numericos consecutivos. ");
+			band=1;
+		}
+		if(letras_consecutivas(cadena))
+		{
+			printf("\nERROR. La contrasenia no debe poseer letras alfabeticamente consecutivas. ");
+			band=1;
+		}
+		if(band==1)
+		{
+			_flushall();
+			printf("\n\nIngrese el contrasenia nuevamente: ");
+			gets(cadena);
+			band=0;letra_minuscula=0;letra_mayuscula=0;caracter_numerico=0;caracter_invalido1=0;
+	
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+
+//retorna verdadero si hay letras consecutivas 
+bool letras_consecutivas(char contrasenia[32])
+{
+	
+	int n=0,i=0, ascii=0,ascii1=0;
+	char letra, letra1,contrasenia2[32];
+	
+	contrasenia2[32]=contrasenia[32];
+	
+	n=strlen(contrasenia2);
+	strlwr(contrasenia2);
+	
+	while(i<n-1)
+	{
+		
+		letra=contrasenia2[i];
+		
+		//validacion de letras	
+		if(letra >= 'a' and letra <=  'z')
+		{
+			ascii=letra;
+			letra1=contrasenia2[i+1];
+			ascii1=letra1;
+		
+			if(ascii+1==ascii1)
+			{
+				return true;
+			}
+		}
+		
+		i++;
+	}
+	
+	return false;	
+}
+
+
+//retorna verdadero si hay numeros consecutivos 
+bool numeros_consecutivos(char contrasenia[32])
+{
+	
+	int n=0,i=0, caracter_numerico=0;
+	
+	n=strlen(contrasenia);
+	
+	while(i<n-1)
+	{
+		if( contrasenia[i]>= '0' and contrasenia[i] <= '9')
+		{
+			caracter_numerico++;
+		}
+		
+		if(caracter_numerico>3)
+		{
+			return true;
+		}
+		
+		i++;
+	}
+	
+	return false;	
 }
