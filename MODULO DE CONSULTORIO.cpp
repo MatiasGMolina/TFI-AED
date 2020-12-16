415b794399debe4245f1bd48aa7803be8de3a7ee
matiasmolina <matias.molina@alu.frt.utn.edu.ar>
2020-12-16 00:18:13 -0300

    Creacion de segunda, tercera y cuarta opción.

--- MODULO DE CONSULTORIO.cpp
+++ MODULO DE CONSULTORIO.cpp
@@ -7,12 +7,37 @@
 void menu();
 bool buscar_vet();
 void lista_espera();
+void evolucion();
 
 struct veterinario
 {	char apell_nom[60];
 	int matricula, dni;
 	char telefono[10];
-	char contrasenia[10];
+	char contrasenia[32];
+};
+struct Fecha
+{
+    int dia;
+    int mes;
+    int year; 
+};
+struct Mascota
+{
+	char apell_nomb[60];
+	char Domicilio[60];
+	int DNI_Duenio;
+	char Locaidad[60];
+    Fecha fecha_nacimiento;
+	float Peso;	
+	char Telefono[25];	
+};
+struct Turnos
+{
+	int Matricula_de_Veterinario;
+	Fecha fecha_cargada;
+	int DNI_Duenio;
+	char Detalle_de_Atencion[380];
+	
 };
 veterinario reg;
 main()
@@ -49,15 +74,11 @@ main()
 					system("\n\npause");
 					break;
 				case 3: 
-					
+						evolucion();
 					
 					system("\n\npause");
 					break;
 				case 4:
-
-					
-					break;
-				case 5:
 					printf("\nGRACIAS POR UTILIZAR NUESTROS SERVICIOS\n\n");
 					
 					break;
@@ -66,10 +87,9 @@ main()
 					printf("\nNo corresponde a ninguna opción del MENU\n\n");system("\n\npause");
 					break;
 			}
-	}while(opcion!=5);
+	}while(opcion!=4);
 
 	printf("\n\n");
-	system("pause");
 }
 void menu()
 {
@@ -146,6 +166,73 @@ bool buscar_vet()
 //lista de espera de turnos(informe)
 void lista_espera()
 {
-	printf("\nhoalllllllllllllllllll.");
+	Fecha hoy;
+	Turnos reg;
+	FILE*arch;
+	printf("\nINFORME DE TURNOS DIARIOS\n");
+	printf("INGRESE EL DIA DEL TURNO:");
+	scanf("%d", &hoy.dia);
+	printf("\nINGRESE EL MES DEL TURNO:");
+	scanf("%d", &hoy.mes);
+	printf("\nINGRESE EL AÑO:");
+	scanf("%d", &hoy.year);
+	arch=fopen("Turnos.dat", "rb");
+	fread(&reg, sizeof(reg),1, arch);
+	while(!feof(arch))
+	{	if(reg.fecha_cargada.dia==hoy.dia && reg.fecha_cargada.mes==hoy.mes &&reg.fecha_cargada.year==hoy.year)
+		{
+				printf("\nMATRICULA VETERINARIO:%d", reg.Matricula_de_Veterinario);
+				printf("\nFECHA DE ATENCION:%d/%d/%d", hoy.dia, hoy.mes, hoy.year);
+				printf("\nDNI DEL DUENIO:%d", reg.DNI_Duenio); 
+				printf("\nDETELLE ATENCION:%s\n", reg.Detalle_de_Atencion);
+		}
+		fread(&reg, sizeof(reg),1, arch);
+	}
+	fclose(arch);
 }
-
+void evolucion()
+{	Fecha hoy;
+	Turnos reg;
+	FILE*arch;
+	long posi;
+	bool h=false;
+	int dni;
+	printf("\nINFORME DE TURNOS DIARIOS\n");
+	printf("INGRESE EL DIA DEL TURNO:");
+	scanf("%d", &hoy.dia);
+	printf("\nINGRESE EL MES DEL TURNO:");
+	scanf("%d", &hoy.mes);
+	printf("\nINGRESE EL AÑO:");
+	scanf("%d", &hoy.year);
+	printf("\nINGRESE EL DNI DEL DUEÑO:");
+	scanf("%d", &dni); 
+	arch=fopen("Turnos.dat", "r+b");
+	fread(&reg, sizeof(reg),1, arch);
+	while(!feof(arch)&&h==false)
+	{	if(reg.fecha_cargada.dia==hoy.dia && reg.fecha_cargada.mes==hoy.mes &&reg.fecha_cargada.year==hoy.year && reg.DNI_Duenio)
+		{
+				h=true;
+				printf("\nDATOS DEL TURNO:");
+				printf("\nMATRICULA VETERINARIO:%d", reg.Matricula_de_Veterinario);
+				printf("\nFECHA DE ATENCION:%d/%d/%d", hoy.dia, hoy.mes, hoy.year);
+				printf("\nDNI DEL DUENIO:%d", reg.DNI_Duenio); 
+				printf("\nDETELLE ATENCION:%s\n", reg.Detalle_de_Atencion);
+				posi=ftell(arch)-sizeof(reg);
+		} else 
+		fread(&reg, sizeof(reg),1, arch);
+	}
+	if(h==false)
+	{	printf("\nMASCOTA NO HALLADA");
+	} else 
+	{	char texto[100];
+		printf("\nINGRESE LA EVOLUCION:");
+		fflush(stdin);
+		gets(texto);
+		strcat(reg.Detalle_de_Atencion, "\n");
+		strcat(reg.Detalle_de_Atencion, texto);
+		fseek(arch, posi, SEEK_SET);
+		fwrite(&reg, sizeof(reg), 1, arch);
+		printf("\nEVOLUCION REGISTRADA SATISFACTORIAMENTE");
+	}
+	fclose(arch);
+}
